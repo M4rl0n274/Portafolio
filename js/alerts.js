1f992ba847810f1c1e2d138382fa35e4088191ae
M4rl0n274 <marlonquinteri274@gmail.com>
2025-02-05 23:36:56 -0500

    se agrega secciones acerca de mi y formulario funcional

---
+++ js/alerts.js
@@ -0,0 +1,16 @@
+
+
+// document.getElementById('form').addEventListener('submit', function(event) {
+
+//     event.preventDefault();
+
+ 
+//     alert('Formulario enviado con éxito');
+// });
+
+
+
+
+
+
+

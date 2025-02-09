408b6d1548f68b56ef238c10be7ecd0406edd292
M4rl0n274 <marlonquinteri274@gmail.com>
2025-02-09 02:40:26 -0500

    se arregla nav bar y animaciones de las cards

--- js/alerts.js
+++ js/alerts.js
@@ -8,9 +8,33 @@
 //     alert('Formulario enviado con éxito');
 // });
 
+function abrirPDF() {
+    window.open("/assets/pdf/Template CV-Generation-Febrero2025.pdf", "_blank");
+}
+
+
+function github(){
+    window.open("https://github.com/M4rl0n274", "_blank");
+}
+
+function Linkedin(){
+    window.open("https://www.linkedin.com/in/marlon-quintero-solano-developer-full-stack6a0180329/" , "_blank")
+}
 
 
 
 
-
-
+$(document).ready(function() {
+    $(document).scroll(function(){
+        let windowHeight = $(window).height()*0.8;
+        // Recorrer cada elemento
+        $('.animated').each(function(i) {
+            // Analizar posición del elemento actual
+            if($(document).scrollTop() >= $(this).offset().top - windowHeight) {
+                $(this).addClass('animation-in');
+            } else {
+                $(this).removeClass('animation-in');
+            }
+        });
+    });
+});

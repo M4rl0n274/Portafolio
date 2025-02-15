b2828e2473569eabeb8096632699a17afabc012b
M4rl0n274 <marlonquinteri274@gmail.com>
2025-02-15 01:29:44 -0500

    actualizacion general de colores, estilo y disposicion de los elementos. Se agrega informacion

--- js/alerts.js
+++ js/alerts.js
@@ -1,26 +1,40 @@
-
-
 // document.getElementById('form').addEventListener('submit', function(event) {
 
 //     event.preventDefault();
 
- 
 //     alert('Formulario enviado con éxito');
 // });
 
 function abrirPDF() {
-    window.open("/assets/pdf/Template CV-Generation-Febrero2025.pdf", "_blank");
+  window.open("/assets/pdf/Template CV-Generation-Febrero2025_2.pdf", "_blank");
 }
 
-
-function github(){
-    window.open("https://github.com/M4rl0n274", "_blank");
+function github() {
+  window.open("https://github.com/M4rl0n274", "_blank");
 }
 
-function Linkedin(){
-    window.open("https://www.linkedin.com/in/marlon-quintero-solano-developer-full-stack6a0180329/" , "_blank")
+function Linkedin() {
+  window.open(
+    "https://www.linkedin.com/in/marlon-quintero-solano-developer-full-stack6a0180329/",
+    "_blank"
+  );
 }
 
+//* Animacion texto
 
+const words = ["desarrollador Web", "Marlon"];
+let index = 0;
+const textElement = document.querySelector(".text");
 
+function updateText() {
+  textElement.style.animation = "none"; // Reset animación
+  textElement.textContent = words[index];
+  void textElement.offsetWidth; // Forzar reflow para reiniciar animación
+  textElement.style.animation =
+    "typing 2s steps(15) forwards, fadeOut 0.5s 2s forwards";
 
+  index = (index + 1) % words.length;
+  setTimeout(updateText, 3000);
+}
+
+updateText();

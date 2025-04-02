bb1bf4a2ee283077b3d1c95779b79c71d18827c8
M4rl0n274 <marlonquinteri274@gmail.com>
2025-04-01 21:56:04 -0500

    agrgada la carga dinamica den el portafolio y proyectos

---
+++ js/proyects.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded",function(){
+
+    fetch("Proyectos.html")
+    .then((response)=> response.text())
+    .then((data)=> {
+        document.getElementById("proyectsCards").innerHTML = data;
+    })
+    .catch((error) => console.error("Error al cargar proyectos", error))
+})
